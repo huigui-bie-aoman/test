885e1d9da984161f1837d21bfce510fbb71139af
antifragile <xxx>
2025-06-12 19:04:55 +0800

    gui first

--- Hui.java
+++ Hui.java
@@ -2,6 +2,6 @@ public class Hui {
     public static void main(String[] args) {
         System.out.println("huigui");
         System.out.println("1");
-        System.out.println("3");
+        System.out.println("6");
     }
 }
